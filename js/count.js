ede3539aab732c1d16bc7a63d148652d284f237f
Edvdvlies <edvdvlies@gmail.com>
2020-05-07 16:54:17 +0200

    timer

--- js/count.js
+++ js/count.js
@@ -2,7 +2,7 @@ function randomNumber(min, max) {
    return Math.floor(Math.random() * (max - min) + min);
 }
 function foo() {
-setTimeout(foo, 10000);
+setTimeout(foo, 18000000);//5uur
 document.getElementById("aantalFans").innerHTML = randomNumber(50, 150);
 }
 foo();
